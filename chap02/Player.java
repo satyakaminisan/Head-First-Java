5c2354dfd8b9aecaab468165b5f841b71fa8ba58
satyakaminisan <satyakamini.mishra@accenture.com>
2025-01-22 08:19:46 +0000

    added comments on Player.java in chap02

--- chap02/Player.java
+++ chap02/Player.java
@@ -1,10 +1,14 @@
 package chap02;
 
 public class Player {
+    // This variable holds the player's guess number
     int number = 0;
-    public void guess()
-    {
+
+    // This method generates a random guess number between 0 and 9 (inclusive)
+    public void guess() {
+        // Generate a random number between 0 and 9 and assign it to the number variable
         number = (int) (Math.random() * 10);
+        // Print out the guessed number to the console
         System.out.println("I'm guessing " + number);
     }
 }
